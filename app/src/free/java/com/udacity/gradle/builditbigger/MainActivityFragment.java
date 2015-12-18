05a91c330b0fd1a9007b9498c0afec907851ceb7
Fabien <vorillion@adipso.com>
2015-12-18 09:57:22 +0100

    add: - Add Interstitial Ad   ->Display the ad after the user hits the button, and before the joke is shown.

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,8 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 /**
@@ -18,6 +21,7 @@ public class MainActivityFragment extends Fragment implements
         IMainActivityLoadable {
 
     private ProgressBar pb_loading;
+    private InterstitialAd mInterstitialAd;
 
     public MainActivityFragment() {
     }
@@ -28,27 +32,58 @@ public class MainActivityFragment extends Fragment implements
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
         pb_loading = (ProgressBar)root.findViewById(R.id.pb_loading);
-
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
+
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("97CFBD5D29F4E15FD8156F67292EFFC6")//(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+
+        //pre-load interstitial
+        mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+
+                //
+            }
+        });
+        requestNewInterstitial();
+
+
         return root;
     }
 
 
     @Override
     public void startLoading() {
+        //show loader
         if (pb_loading != null)
             pb_loading.setVisibility(View.VISIBLE);
+
+        //show interstitial if loaded
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
     }
     @Override
     public void stopLoading() {
+        //hide loader
         if (pb_loading != null)
             pb_loading.setVisibility(View.GONE);
+    }
+
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mInterstitialAd.loadAd(adRequest);
     }
 }
